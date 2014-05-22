5e7620e0c865be0294a5d95c18c3cf055617f939
Michael Pearson <mipearson@gmail.com>
2014-05-22 21:47:59 +1000

    Add cdnjs.
    Fixes #3

--- site-cookbooks/nan-nginx/recipes/default.rb
+++ site-cookbooks/nan-nginx/recipes/default.rb
@@ -19,4 +19,21 @@ cookbook_file "/etc/nginx/sites-enabled/nan.conf" do
   notifies :restart, "service[nginx]"
 end
 
+git "/opt/html/cdnjs" do
+  repository "https://github.com/cdnjs/cdnjs.git"
+  action :checkout
+  depth 1
+end
+
+git "/opt/html/cdnjs-web" do
+  repository "https://github.com/cdnjs/website.git"
+  action :checkout
+  depth 1
+end
+
+cookbook_file "/etc/nginx/sites-enabled/cdnjs.conf" do
+  notifies :restart, "service[nginx]"
+end
+
+
 
